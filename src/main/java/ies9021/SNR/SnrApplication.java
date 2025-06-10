aee4e81911a8545dc5d05f5f1b679f40570aa53f
Ulises Rosales <ulises223366@gmail.com>
2025-06-09 21:33:03 -0300

    Commit base

---
+++ src/main/java/ies9021/SNR/SnrApplication.java
@@ -0,0 +1,13 @@
+package ies9021.SNR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ies9021/SNR/SnrApplicationTests.java
@@ -0,0 +1,13 @@
+package ies9021.SNR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
